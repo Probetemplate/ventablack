2804c3c223b8b753ad29757ccf8dd16f9fa68934
Probetemplate <147812661+Probetemplate@users.noreply.github.com>
2024-03-06 11:39:37 +0600

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import FirebaseApp from "$lib/components/FirebaseApp.svelte";
-  import { db as firestore, auth, rtdb, storage, analytics } from "./firebase.js";
+  import { db as firestore, auth, rtdb, storage, analytics } from "./firebase.ts";
 </script>
 
 <FirebaseApp {auth} {firestore} {rtdb} {storage} {analytics}>
