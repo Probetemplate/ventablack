51e3c48b4c32b0a5740f70ed0437dd2622f92ce6
Probetemplate <147812661+Probetemplate@users.noreply.github.com>
2024-03-29 15:38:15 +0600

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
-import {onMount} from 'svelte';
+import { app } from "./firebase";
+import { fauth } from "./firebase";
+import { fstore } from "./firebase";
+import { db } from "./firebase";
 </script>
 
   <slot />
